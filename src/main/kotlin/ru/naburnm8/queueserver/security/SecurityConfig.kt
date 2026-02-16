0b636d4ffd2eb4616f943d060803992aaa3c656f
Me <alint@bk.ru>
2026-02-16 19:34:42 +0300

    add SecurityConfig.kt and local properties support

---
+++ src/main/kotlin/ru/naburnm8/queueserver/security/SecurityConfig.kt
@@ -0,0 +1,11 @@
+package ru.naburnm8.queueserver.security
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity
+
+@Configuration
+@EnableMethodSecurity
+class SecurityConfig (
+
+) {
+}
