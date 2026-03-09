023a3a68962ffa81d3b2478d22e3add66777cca4
naburnm8 <rtender.artem@gmail.com>
2026-03-09 21:58:31 +0300

    refactor IntegrationController by removing unused imports and dependencies

--- src/main/kotlin/ru/naburnm8/queueserver/integration/controller/IntegrationController.kt
+++ src/main/kotlin/ru/naburnm8/queueserver/integration/controller/IntegrationController.kt
@@ -1,28 +1,20 @@
 package ru.naburnm8.queueserver.integration.controller
 
-import org.springframework.http.HttpStatus
-import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
-import org.springframework.web.client.RestTemplate
-import ru.naburnm8.queueserver.integration.body.IntegrationPayloadBody
-import ru.naburnm8.queueserver.integration.repository.IntegrationRepository
 import ru.naburnm8.queueserver.integration.response.IntegrationResponse
 import ru.naburnm8.queueserver.integration.service.IntegrationService
 import ru.naburnm8.queueserver.profile.request.RegisterStudentRequest
-import tools.jackson.databind.ObjectMapper
 import java.util.UUID
 
 @RestController
 @RequestMapping("/api/auth/integration")
 class IntegrationController (
     private val integrationService: IntegrationService,
-    private val integrationRepository: IntegrationRepository,
-    private val objectMapper: ObjectMapper
 
 ) {
 
